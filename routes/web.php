c330139130793b3a07347f66752a93b0a2aaef5a
Bidyut-CSM <onlinemessages0001@gmail.com>
2023-06-30 14:26:33 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\BlogController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/ 
+*/  
 //git status
 //git add .
 //git commit -m "second commit"
